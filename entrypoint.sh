d39cc80fc3ea0a4bf299c5fd9cf288da03e3c801
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-15 23:03:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo "Environment variables:"
 echo "PLEX_HOSTNAME=${PLEX_HOSTNAME}"
 echo "PLEX_TOKEN=${PLEX_TOKEN}"
 echo "CRON_EXPRESSION=${CRON_EXPRESSION}"
+echo "DEADLINE=${DEADLINE}"
 echo "################################"
 echo -e "${CRON_EXPRESSION//\ /\\t}\t/autopoweroff.sh >> /autopoweroff.log 2>&1" >> /var/spool/cron/crontabs/root
 exec sh -c "crond && tail -F /autopoweroff.log"
