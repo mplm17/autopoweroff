b6648808f78a72515703bb3e7f5722c3f7a74d3d
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-15 16:55:09 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,4 @@ echo "PLEX_HOSTNAME=${PLEX_HOSTNAME}"
 echo "PLEX_TOKEN=${PLEX_TOKEN}"
 echo "CRON_EXPRESSION=${CRON_EXPRESSION}"
 echo "################################"
-touch /autopoweroff.log
 echo -e "${CRON_EXPRESSION//\ /\\t}\t/autopoweroff.sh >> /autopoweroff.log" >> /var/spool/cron/crontabs/root
-exec crond && tail -F /autopoweroff.log
