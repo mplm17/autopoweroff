dec79a9745c701defd25f4c72b4e2f289439e53b
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-15 02:30:49 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo "Variables:"
 echo "PLEX_HOSTNAME=${PLEX_HOSTNAME}"
 echo "PLEX_TOKEN=${PLEX_TOKEN}"
 echo "CRON_EXPRESSION=${CRON_EXPRESSION}"
-echo "${CRON_EXPRESSION} /autopoweroff.sh" | crontab -u $(whoami) -
+exec /autopoweroff.sh
