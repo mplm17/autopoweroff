210f06caf8096a5747a36a15bb60f906fee3c75b
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-16 00:09:38 +0100

    Update autopoweroff.sh

--- autopoweroff.sh
+++ autopoweroff.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-echo "Running autopoweroff.sh"
-date +"%T"
-curl http://${PLEX_HOSTNAME}:32400/status/sessions?X-Plex-Token=${PLEX_TOKEN}  -f -s | grep "<MediaContainer" | grep -o "\".*\"" | tr -d '"'
-echo "End of autopoweroff.sh"
+
+TIME=$(date +"%T")
+
+echo "$(date) - Checking connected users on Plex..."
+CONNECTED_USERS=$(curl http://${PLEX_HOSTNAME}:32400/status/sessions?X-Plex-Token=${PLEX_TOKEN}  -f -s | grep "<MediaContainer" | grep -o "\".*\"" | tr -d '"')
+
+if [[ -z "$CONNECTED_USERS" ]]; then
+  echo "$(date) - Cannot get number of connected users, do nothing..."
+elif [[ $CONNECTED_USERS -eq 0 ]]; then
+  echo "$(date) - 0 user is connected on Plex, host shutdown..."
+  systemctl poweroff
+elif [[ $CONNECTED_USERS -eq 1 ]]; then
+  echo "$(date) - 1 user is connected on Plex, do nothing..."
+elif [[ $CONNECTED_USERS -gt 1 ]]; then
+  echo "$(date) - ${CONNECTED_USERS} users are connected on Plex, do nothing..."
+fi
+
+if [[ $TIME == $DEADLINE ]]; then
+  echo "$(date) - Deadline reached, host shutdown with ${CONNECTED_USERS} user(s)..."
+  systemctl poweroff
+fi
