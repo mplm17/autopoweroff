de509957254b5bebc98409c2043e6a4e668e508a
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-15 02:21:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo "################################"
 echo "Variables:"
 echo "PLEX_HOSTNAME=${PLEX_HOSTNAME}"
 echo "PLEX_TOKEN=${PLEX_TOKEN}"
-echo "PLEX_TOKEN=${PLEX_TOKEN}"
-exec curl http://${PLEX_HOSTNAME}:32400/status/sessions?X-Plex-Token=${PLEX_TOKEN}  -f -s | grep "<MediaContainer" | grep -o "\".*\"" | tr -d '"'
+echo "CRON_EXPRESSION=${CRON_EXPRESSION}"
+"${CRON_EXPRESSION} /autopoweroff.sh" | crontab -
