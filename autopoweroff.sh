bba41b2b550fe2f1277e68eb4b490c691b7cf8f1
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-15 16:16:19 +0100

    Update autopoweroff.sh

--- autopoweroff.sh
+++ autopoweroff.sh
@@ -2,6 +2,6 @@
 echo "test"
 date +"%T"
 curl http://${PLEX_HOSTNAME}:32400/status/sessions?X-Plex-Token=${PLEX_TOKEN}  -f -s | grep "<MediaContainer" | grep -o "\".*\"" | tr -d '"'
-${whoami}
+echo "$USER"
 sleep 10
 date +"%T"
