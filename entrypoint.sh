985338433c572a017e302e58ee57b1d3dab40401
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-16 09:56:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "################################"
-echo "Starting autopoweroff container."
-echo "################################"
+echo "################################################################"
+echo "$(date) - Loading autopoweroff..."
+echo "################################################################"
 echo "Environment variables:"
 echo "PLEX_HOSTNAME=${PLEX_HOSTNAME}"
 echo "PLEX_TOKEN=${PLEX_TOKEN}"
 echo "CRON_EXPRESSION=${CRON_EXPRESSION}"
 echo "DEADLINE=${DEADLINE}"
-echo "################################"
+echo "################################################################"
 printenv > /var/spool/cron/crontabs/root
 echo "${CRON_EXPRESSION} /autopoweroff.sh >> /autopoweroff.log 2>&1" >> /var/spool/cron/crontabs/root
 crontab /var/spool/cron/crontabs/root
