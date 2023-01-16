980cc6ace1d7e0e6b971d3fe94686566f6303b97
mplm17 <39955446+mplm17@users.noreply.github.com>
2023-01-16 01:23:15 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ echo "PLEX_TOKEN=${PLEX_TOKEN}"
 echo "CRON_EXPRESSION=${CRON_EXPRESSION}"
 echo "DEADLINE=${DEADLINE}"
 echo "################################"
-echo "${CRON_EXPRESSION} /autopoweroff.sh >> /autopoweroff.log 2>&1" > /var/spool/cron/crontabs/root
+echo "CRON_TZ=${TZ}" > /var/spool/cron/crontabs/root
+echo "${CRON_EXPRESSION} /autopoweroff.sh >> /autopoweroff.log 2>&1" >> /var/spool/cron/crontabs/root
 crontab /var/spool/cron/crontabs/root
 exec bash -c "cron && tail -F /autopoweroff.log"
